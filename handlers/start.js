7eb92b9833a6b1839f9b2b257ed93f1fe5b24917
Pratik Patil <Pratik7230@GMAIL.COM>
2026-07-30 18:52:25 +0530

    Enhance welcome message

--- handlers/start.js
+++ handlers/start.js
@@ -2,6 +2,12 @@ const languageKeyboard = require("../keyboards/language");
 
 module.exports = (bot) => {
   bot.start((ctx) => {
-    ctx.reply("😂 Welcome!\n\nSelect your language.", languageKeyboard);
+    const userName = ctx.from.first_name ? ctx.from.first_name : "there";
+    const welcomeMessage = `🎉 <b>Welcome to the Joke Bot, ${userName}!</b> 🎉\n\nI'm here to bring some laughter into your day. 😂\nGet ready for some hilarious jokes!\n\n👇 <b>Please select your preferred language below to get started:</b>`;
+    
+    ctx.reply(welcomeMessage, {
+      parse_mode: "HTML",
+      ...languageKeyboard,
+    });
   });
 };
